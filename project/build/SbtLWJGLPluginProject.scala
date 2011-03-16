5a909e17664435c0ce584e74bed9dff24b5a9aaf
Philip Cali <philip.cali@gmail.com>
2011-03-16 17:24:24 -0500

    Removed unit test libs.

--- project/build/SbtLWJGLPluginProject.scala
+++ project/build/SbtLWJGLPluginProject.scala
@@ -1,6 +1,4 @@
 import sbt._
 
 class SbtLWGJLPluginProject(info: ProjectInfo) extends PluginProject(info) {
-  val testVersion = "1.1"
-  val scalatest = "org.scalatest" % "scalatest" % testVersion % "test" 
 }
