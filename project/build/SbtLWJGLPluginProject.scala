d89f21dda7873fd7354d89e65243c41f41a967c8
Philip Cali <philip.cali@gmail.com>
2011-03-16 18:25:13 -0500

    Added scala tools publishing

--- project/build/SbtLWJGLPluginProject.scala
+++ project/build/SbtLWJGLPluginProject.scala
@@ -1,4 +1,9 @@
 import sbt._
 
 class SbtLWGJLPluginProject(info: ProjectInfo) extends PluginProject(info) {
+  override def managedStyle = ManagedStyle.Maven
+  val publishTo = "Scala Tools Nexus" at
+                  "http://nexus.scala-tools.org/content/repositories/releases/"
+
+  Credentials(Path.userHome / ".ivy2" / ".credentials", log)
 }
