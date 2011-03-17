bafaa06e6f0fd1fb6a82826389d17959902ee0d2
Tom Streller <dusklykan@yahoo.co.uk>
2011-03-17 18:04:25 +0100

    Correct files now

--- src/main/scala/LWJGLProject.scala
+++ src/main/scala/LWJGLProject.scala
@@ -1,77 +1,4 @@
 import sbt._
-<<<<<<< HEAD
-import java.io._
-import java.util.jar._
-
-object JarExtractor {
-	private def copyStream(istream: InputStream, ostream: OutputStream) : Unit = {
-		var bytes =  new Array[Byte](1024)
-		var len = -1
-		while({ len = istream.read(bytes, 0, 1024); len != -1 })
-		ostream.write(bytes, 0, len)
-	}
-
-	def extractJar(file: File, directory: String) : Unit = {
-		val basename = file.getName.substring(0, file.getName.lastIndexOf("."))
-		val todir = if(directory == "") new File(file.getParentFile, basename) else new File(directory + "/" + basename)
-		todir.mkdirs()
-
-		println("Extracting " + file + " to " + todir)
-		val jar = new JarFile(file)
-		val enu = jar.entries
-		while(enu.hasMoreElements) {
-			val entry = enu.nextElement
-			val entryPath = if(entry.getName.startsWith(basename)) entry.getName.substring(basename.length)
-				else entry.getName
-
-			println("Extracting to " + todir + "/" + entryPath)
-			if(entry.isDirectory) {
-				new File(todir, entryPath).mkdirs
-			} else {
-				val istream = jar.getInputStream(entry)
-				val ostream = new FileOutputStream(new File(todir, entryPath))
-				copyStream(istream, ostream)
-				ostream.close
-				istream.close
-			}
-		}
-	}
-
-	def apply(filename: String): Unit = extractJar(new File(filename), "")
-
-	def apply(filename: String, directory: String): Unit = extractJar(new File(filename), directory)
-}
-
-abstract class LWJGLProject(info: ProjectInfo) extends DefaultProject(info) {
-	def lwjglVersion = "2.6"
-
-	lazy val lwjglRepo = "lwjgl" at "http://adterrasperaspera.com/lwjgl"
-
-	lazy val lwjgl = "org.lwjgl" % "lwjgl" % lwjglVersion
-	lazy val lwjglUtil = "org.lwjgl" % "lwjgl-util" % lwjglVersion
-
-	def defineOs = System.getProperty("os.name").toLowerCase.take(3).toString match {
-		case "lin" => ("linux", ":")
-		case "mac" => ("macosx", ":")
-		case "win" => ("windows", ";")
-		case "sun" => ("solaris", ":")
-		case _ => ("unknown", "")
-	}
-
-	def nativeLWJGLPath = {
-		val (libpath, separator) = defineOs._1 match {
-		case "unknown" => ("", "")
-		case _ => (path("lib") / ("lwjgl-native-" + lwjglVersion) / defineOs._1, defineOs._2)
-		}
-
-		System.getProperty("java.library.path") + separator + libpath
-	}
-
-	override def fork = {
-		JarExtractor((managedDependencyPath / "compile" / ("lwjgl-native-" + lwjglVersion + ".jar")).toString, dependencyPath.toString)
-		forkRun(("-Djava.library.path=" + nativeLWJGLPath) :: Nil)
-	}
-=======
 import scala.io.Source
 
 abstract class LWJGLProject(info: ProjectInfo) extends DefaultProject(info) { 
@@ -109,7 +36,6 @@ abstract class LWJGLProject(info: ProjectInfo) extends DefaultProject(info) {
     }
     forkRun(("-Djava.library.path=" + nativeLWJGLPath) :: Nil)
   }
->>>>>>> 1767012ede7a1a5e3b929c558993b2ca0b09180f
 }
 
 // Slick works a tad differently
