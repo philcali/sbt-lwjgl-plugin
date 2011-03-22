93ea91eaca794a83cc3f7aa8c494b8e91a14504f
Philip Cali <philip.cali@gmail.com>
2011-03-21 22:13:05 -0500

    Cleaned logic a bit. Made Slick2D and jMonkey more intelligent.

--- src/main/scala/LWJGLProject.scala
+++ src/main/scala/LWJGLProject.scala
@@ -1,10 +1,16 @@
 import sbt._
-import java.util.regex.Pattern
-import java.io.FileNotFoundException
+
 import scala.io.Source
+import java.net.URL
+import java.util.regex.Pattern
+import java.io.{ FileNotFoundException, FileOutputStream }
 
-trait LWJGLForkRun extends DefaultProject {
+abstract class LWJGLProject(info: ProjectInfo) extends DefaultProject(info) { 
+	lazy val lwjglRepo = "Diablo-D3" at "http://adterrasperaspera.com/lwjgl"
 
+	lazy val lwjgl = "org.lwjgl" % "lwjgl" % lwjglVersion
+	lazy val lwjglUtils = "org.lwjgl" % "lwjgl-util" % lwjglVersion
+ 
 	private lazy val defineOs = System.getProperty("os.name").toLowerCase.take(3).toString match {
 		case "lin" => ("linux", ":", "so")
 		case "mac" => ("macosx", ":", "lib")
@@ -40,7 +46,8 @@ trait LWJGLForkRun extends DefaultProject {
 
   // Children must override this definition in order
   // for the ForkRun trait to be effective
-  def lwjglJar: String
+	def lwjglJar = "lwjgl-native-%s" format(lwjglVersion)
+	def lwjglVersion = "2.7.1"
 
 	override def copyResourcesAction = super.copyResourcesAction dependsOn copyLwjgl
 
@@ -55,17 +62,7 @@ trait LWJGLForkRun extends DefaultProject {
 	override def fork = {
 		forkRun(("-Djava.library.path=" + nativeLWJGLPath) :: Nil)
 	}
-}
 
-abstract class LWJGLProject(info: ProjectInfo) extends DefaultProject(info) with LWJGLForkRun { 
-	lazy val lwjglRepo = "Diablo-D3" at "http://adterrasperaspera.com/lwjgl"
-
-	lazy val lwjgl = "org.lwjgl" % "lwjgl" % lwjglVersion
-	lazy val lwjglUtils = "org.lwjgl" % "lwjgl-util" % lwjglVersion
- 
-  // Force override for ForkRun trait 
-	def lwjglJar = "lwjgl-native-%s" format(lwjglVersion)
-	def lwjglVersion = "2.7.1"
 }
 
 /**
@@ -73,16 +70,42 @@ abstract class LWJGLProject(info: ProjectInfo) extends DefaultProject(info) with
  * We are going to hack the dependency by pulling down their
  * nightly builds and extracting the dependecies we need.
  */
-import dispatch._
-import Http._
-import java.io.FileOutputStream
-
-abstract class JMonkeyProject(info: ProjectInfo) extends DefaultProject(info) with LWJGLForkRun {
+trait JMonkey extends LWJGLProject {
   lazy val baseRepo = "http://jmonkeyengine.com/nightly" 
   lazy val jname = "%s_%s" format(jmonkeyBaseVersion, targetedVersion)
 
-  // This is the jar with teh goods
-  def lwjglJar = "%s-lwjgl-natives" format(jmonkeyBaseVersion)
+  // Bulk of the work, any exception here can
+  // bubble up to the updateAction
+  lazy val updateJmonkey = task {
+    dependencyPath / jname exists match {
+      case true => 
+        log.info("Already have %s" format(jname))
+        None
+      case false =>
+        // If they wanted a nightly build then this could get extreme
+        log.info("Cleaning older versions of %s" format(jmonkeyBaseVersion))
+        val previousVersions = dependencyPath * "%s*".format(jmonkeyBaseVersion) 
+        FileUtilities.clean(previousVersions.get, log)
+
+        // Start the download
+        log.info("Pulling %s" format(jname))
+        log.warn("This may take a few minutes...")
+        val zip = "%s.zip" format(jname) 
+        val dest = dependencyPath / jname
+        
+        // Comencing work...
+        val zipFile = new java.io.File(zip)
+        val url = new URL("%s/%s" format(baseRepo, zip))
+        FileUtilities.download(url, zipFile, log) 
+        // Extract the lib dir only...
+        val filter = new PatternFilter(Pattern.compile(".*jar"))
+        FileUtilities.unzip(zipFile, dest, filter, log)
+        // Destroy the zip
+        zipFile.delete
+        log.info("Complete")
+        None
+    } 
+  } describedAs "Pulls jMonkey dependency from nightly build."
 
   // Giving the ability for users to override
   // the base version and targeted nightly build
@@ -96,53 +119,25 @@ abstract class JMonkeyProject(info: ProjectInfo) extends DefaultProject(info) wi
     sdf.format(when)
   }
 
-  // Bulk of the work, any exception here can
-  // bubble up to the updateAction
-  def pullLib = {
-    dependencyPath / jname exists match {
-      case true => log.info("Already have targeted jMonkey Version")
-      case false =>
-        log.info("Pulling new version of jMonkey")
-        log.warn("This may take a few minutes...")
-        val zip = "%s.zip" format(jname) 
-        val dest = dependencyPath / jname
-        
-        // Comencing work...
-        Http("%s/%s".format(baseRepo, zip) >>> new FileOutputStream(zip))
-        val zipFile = new java.io.File(zip)
-        // Extract the lib dir only...
-        val filter = new PatternFilter(Pattern.compile(".*jar"))
-        FileUtilities.unzip(zipFile, dest, filter, log)
-        // Destroy the zip
-        zipFile.delete
-        log.info("Complete")
-    }
-  }
-
-  override def updateAction = { pullLib; super.updateAction }
+  override def updateAction = 
+    super.updateAction dependsOn updateJmonkey 
 }
 
-// Slick works a tad differently
-abstract class Slick2D(info: ProjectInfo) extends DefaultProject(info) with LWJGLForkRun {
-	val slickRepo = "Slick2D Maven Repo" at "http://slick.cokeandcode.com/mavenrepo"
+/**
+ * Slick dependencies
+ */
+trait Slick2D extends LWJGLProject { 
+	lazy val slickRepo = "Slick2D Maven Repo" at "http://slick.cokeandcode.com/mavenrepo"
 
 	// Mainly for slick stuff
-	val b2srepo = "b2srepo" at "http://b2s-repo.googlecode.com/svn/trunk/mvn-repo"
-	val freeheprepo = "Freehep" at "http://java.freehep.org/maven2"
+	lazy val b2srepo = "b2srepo" at "http://b2s-repo.googlecode.com/svn/trunk/mvn-repo"
+	lazy val freeheprepo = "Freehep" at "http://java.freehep.org/maven2"
 
-	val slick = "slick" % "slick" % slickVersion 
+	lazy val slick = "slick" % "slick" % slickVersion 
 
-  // Override this for newer version
-  def slickVersion = "274"
-
-  def lwjglJar = "lwjgl-native-2.6"
-
-	override def updateAction = {
-		patchFile
-		super.updateAction
-	}
-
-	private def patchFile = {
+  // Patch unfortunately can't depend on update because
+  // update will fail leaving the file to be patched behind
+	lazy val `patch` = task {
 		val path = "%s/.ivy2/cache/phys2d/phys2d/ivy-060408.xml" format(System.getProperty("user.home"))
 		new java.io.File(path) exists match {
 		case true =>
@@ -154,9 +149,18 @@ abstract class Slick2D(info: ProjectInfo) extends DefaultProject(info) with LWJG
 			writer.write(pattern.replaceAllIn(text, "jar"))
 			writer.close
 			log.info("Done.")
+      None
 		case false =>
 			log.warn("Update might fail. This is expected.")
 			log.warn("Please run update one more time.")
+      None
 		}
-	}
+	} describedAs "Patchs the phys2d dependency xml file"
+
+  // Override this for newer version
+  def slickVersion = "274"
+
+	override def updateAction = 
+		super.updateAction dependsOn `patch`
+
 }
