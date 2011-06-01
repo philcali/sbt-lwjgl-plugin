78adab1beeb2bcf89320708adfa427a7125b9ad4
Philip Cali <philip.cali@gmail.com>
2011-06-01 08:01:49 -0500

    Added Ardor3D and Nicol support

---
+++ src/main/scala/Ardor3D.scala
@@ -0,0 +1,10 @@
+import sbt._
+
+trait Ardor3D extends LWJGLProject {
+  def ardorVersion = "0.7"
+
+  lazy val ardorRepo = "Ardor3D Maven Repo" at 
+                       "http://ardor3d.com:8081/nexus/content/groups/public/"
+
+  lazy val ardor = "com.ardor3d" % "ardor3d" % ardorVersion 
+}
