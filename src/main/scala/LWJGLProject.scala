c8b7c9a2383372dd9a30f1c312c5e143631c4872
Philip Cali <philip.cali@gmail.com>
2011-06-10 18:01:30 -0500

    Finished porting base LWJGL Project settings.

--- src/main/scala/LWJGLProject.scala
+++ src/main/scala/LWJGLProject.scala
@@ -13,6 +13,7 @@ object LWJGLProject extends Plugin {
 
   // Default Settings
   val lwjglCopyDir = SettingKey[RichFile]("lwjgl-copy-location", "This is where lwjgl resources will be copied")
+  val lwjglNativesDir = SettingKey[RichFile]("lwjgl-natives-directory", "This is the location where the lwjgl-natives will bomb to") 
   val lwjglVersion = SettingKey[String]("lwjgl-version", "This is the targeted LWJGL verision")
 
   // Define Tasks
@@ -24,17 +25,12 @@ object LWJGLProject extends Plugin {
 
       val target = dir / os
 
-      val org = "org.lwjgl"
-      val name = "lwjgl-native"
-      val jar = "%s-%s.jar" format(name, lwv)
-
       if(target.exists) {
         s.log.info("Skipping because of existence: %s" format(target))
       } else {
         val filter = new PatternFilter(Pattern.compile(os + "/.*" + ext))
 
-        val targetJar = Path.userHome / ".ivy2" / "cache" / org / name / "jars" / jar
-        IO.unzip(targetJar, dir.asFile, filter)
+        IO.unzip(pullNativeJar(lwv), dir.asFile, filter)
       }
     }
 
@@ -43,6 +39,25 @@ object LWJGLProject extends Plugin {
     (streams, lwjglCopyDir) map { (s, dir) =>
       s.log.info("Cleaning LWJGL files")
       IO.delete(dir / defineOs._1 asFile)
+    }
+
+  val lwjglNatives = TaskKey[Unit]("lwjgl-natives", "Copy LWJGL resources to output directory")
+  private def lwjglNativesTask =
+    (streams, lwjglNativesDir, lwjglVersion) map { (s, outDir, lwv) =>
+      val unzipTo = file(".") / "natives-cache"
+      val lwjglN = pullNativeJar(lwv)
+
+      s.log.info("Unzipping the native jar")
+      IO.unzip(lwjglN, unzipTo)
+
+      val allFiles = unzipTo ** "*.*"
+
+      allFiles.get foreach { f =>
+        IO.copyFile(f, outDir / f.name)
+      }
+      // Delete cache
+      s.log.info("Removing cache")
+      IO.delete(unzipTo.asFile)
     }
 
   // Helper methods 
@@ -54,15 +69,25 @@ object LWJGLProject extends Plugin {
     case _ => ("unknown", "")
   }
 
+  private def pullNativeJar(lwv: String) = { 
+    val org = "org.lwjgl"
+    val name = "lwjgl-native"
+    val jar = "%s-%s.jar" format(name, lwv)
+    Path.userHome / ".ivy2" / "cache" / org / name / "jars" / jar
+  }
+
   // Every project must have these
   override lazy val settings = Seq (
     // Settings
-    lwjglCopyDir := file(".") / "lwjgl-resources",
     lwjglVersion := "2.7.1",
+    lwjglCopyDir := file(".") / "lwjgl-resources",
+    lwjglNativesDir <<= (target) { _ / "lwjgl-natives" }, 
 
     // Tasks and dependencies
     lwjglCopy in update <<= lwjglCopyTask,
-    lwjglCopy <<= Seq(update, lwjglCopy in update).dependOn, 
+    lwjglCopy <<= Seq(update, lwjglCopy in update).dependOn,
+    lwjglNatives in update <<= lwjglNativesTask,
+    lwjglNatives <<= Seq(update, lwjglCopy, lwjglNatives in update).dependOn,
     lwjglClean <<= lwjglCleanTask,
 
     // Neeed to load LWJGL in java.library.path
@@ -79,43 +104,3 @@ object LWJGLProject extends Plugin {
     ) }
   )
 }
-
-/*
-abstract class LWJGLProject(info: ProjectInfo) extends DefaultProject(info) { 
-  lazy val compilePath = managedDependencyPath / "compile"
-
-  lazy val lwjglNatives = task {
-    val unzipTo = lwjglNativeOutputPath / "natives"
-    val lwjglN = compilePath / "%s.jar".format(lwjglJar)
-
-    FileUtilities.unzip(lwjglN, unzipTo, log)
-
-    val allFiles = unzipTo ** "*.*"
-    FileUtilities.copyFlat(allFiles.get, lwjglNativeOutputPath, log)
-    FileUtilities.clean(unzipTo, log)
-    unzipTo.asFile.delete
-    None
-  } dependsOn(`update`) describedAs "Extract lwjgl natives to defined outputPath."
-
-  // Override this to extract libraries somewhere else
-  def lwjglNativeOutputPath = outputPath
-	def lwjglJar = "lwjgl-native-%s".format(lwjglVersion)
-	def lwjglVersion = "2.7.1"
-
-  // Still need to figure out how to do this
-	override def copyResourcesAction = super.copyResourcesAction dependsOn copyLwjgl
-
-  // Removing the java.library.path addition, as
-  // this could only cause the double loading
-  // error... Will revisit this if this implementation
-  // becomes a problem (though I don't see how it could).
-	def nativeLWJGLPath = defineOs._1 match {
-		case "unknown" => ""
-		case _ => nativeLibPath / defineOs._1
-	}
-        
-	override def fork = {
-		forkRun(("-Djava.library.path=" + nativeLWJGLPath) :: Nil
-	}
-}
-*/
